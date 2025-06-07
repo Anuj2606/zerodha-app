45105786ff9b74f2316fa67d7fef23de7ef0596c
Anuj Sawant <anujsawant26@gmail.com>
2025-06-07 17:32:28 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,8 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        {/* Logo */}
         <div className="text-xl font-bold text-blue-600">Zerodha</div>
-
-        {/* Navigation Links */}
+        
         <div className="space-x-4 text-sm text-gray-700">
           <Link to="/signup" className="hover:text-blue-600">Signup</Link>
           <Link to="/about" className="hover:text-blue-600">About</Link>
